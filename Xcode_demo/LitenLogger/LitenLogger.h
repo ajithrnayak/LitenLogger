4df90deed9691a4c6a7fe7f14a5511dc80dc5a2a
Ajith R Nayak <ajithrnayak@icloud.com>
2019-06-24 20:01:41 +0530

    Created a Xcode project that references source file.

---
+++ Xcode_demo/LitenLogger/LitenLogger.h
@@ -0,0 +1,19 @@
+//
+//  LitenLogger.h
+//  LitenLogger
+//
+//  Created by Ajith R Nayak on 2019-06-24.
+//  Copyright © 2019 Ajith R Nayak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LitenLogger.
+FOUNDATION_EXPORT double LitenLoggerVersionNumber;
+
+//! Project version string for LitenLogger.
+FOUNDATION_EXPORT const unsigned char LitenLoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LitenLogger/PublicHeader.h>
+
+
